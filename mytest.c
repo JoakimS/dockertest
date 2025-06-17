e1545fce765f98f58e7b5b2561972c6160be2058
JoakimS <126427499+JoakimS@users.noreply.github.com>
2025-06-17 09:50:25 -0600

    third commit

--- mytest.c
+++ mytest.c
@@ -3,7 +3,7 @@
 int main() {
     printf("<!DOCTYPE html>\n");
     printf("<html><head><title>Hello</title></head><body>\n");
-    printf("<h1>Hello, World from C!</h1>\n");
+    printf("<h1>this is a test from C!</h1>\n");
     printf("</body></html>\n");
     return 0;
 }
